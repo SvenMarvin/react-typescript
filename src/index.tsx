8fccd975bc341b13fe3f71a8fde32c3da310a4e8
Sven Marvin Brüheim <marvin@Air-von-Sven.fritz.box>
2021-03-13 18:32:28 +0100

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
 import Dokumentation from './pages/documentation/documentation';
 import UserData from './pages/userData/userData';
->>>>>>> release/0.0.2
 // Import Global Store
 import {LoginContextContainer} from './store/login/LoginContextContainer';
 // Import Navigation
@@ -16,11 +13,8 @@ const app = (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={App} />
-<<<<<<< HEAD
-=======
       <Route exact path='/docs' component={Dokumentation} />
       <Route exact path='/userData' component={UserData} />
->>>>>>> release/0.0.2
     </Switch>
   </BrowserRouter>
 );
